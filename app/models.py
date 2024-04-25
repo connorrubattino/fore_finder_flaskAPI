31edb14326b4d5556b6168adbb7bfa664e4c32f9
Connor Rubattino <connor.rubattino@gmail.com>
2024-04-25 15:17:29 -0700

    to_dict update

--- app/models.py
+++ app/models.py
@@ -62,11 +62,17 @@ class Golfer(db.Model):
             "phone":self.phone,
             "city": self.city,
             "district": self.district,
-            "country": self.country
+            "country": self.country,
+            "right_handed": self.right_handed,
+            "alchohol": self.alchohol,
+            "legal_drugs": self.legal_drugs,
+            "smoker": self.smoker,
+            "gambler": self.gambler,
+            "music": self.music,
         }
     
     def update(self, **kwargs):
-        allowed_fields = {'first_name', 'last_name', 'city', 'district', 'country', 'email', 'phone', 'handicap', 'golfer_age', 'right_handed', 'alcholol', 'legal_drugs', 'smoker', 'gambler', 'tees'}
+        allowed_fields = {'first_name', 'last_name', 'city', 'district', 'country', 'email', 'phone', 'handicap', 'golfer_age', 'right_handed', 'alcholol', 'legal_drugs', 'smoker', 'gambler', 'tees', 'music'}
         for key, value in kwargs.items():
             if key in allowed_fields:
                 setattr(self, key, value)
