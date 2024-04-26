bf62ae36916a516442eb0a4e6340d44455351ac0
Connor Rubattino <connor.rubattino@gmail.com>
2024-04-26 01:58:34 -0700

    typo

--- app/routes.py
+++ app/routes.py
@@ -209,7 +209,7 @@ def delete_teetime(teetime_id):
 
 
 @app.route('/teetimes<int:teetime_id>/golfer_comments')
-def get_teetimes():
+def get_comments():
     select_stmt = db.select(Golfer_comment)
     comments = db.session.execute(select_stmt).scalars().all()
     return [c.to_dict() for c in comments]
